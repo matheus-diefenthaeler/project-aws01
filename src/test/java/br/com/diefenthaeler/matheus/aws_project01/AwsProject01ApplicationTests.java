1bba26d760573eef9441d9ed4c9775b613fd5633
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-04 23:56:34 -0300

    Test endpoint application

---
+++ src/test/java/br/com/diefenthaeler/matheus/aws_project01/AwsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diefenthaeler.matheus.aws_project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
