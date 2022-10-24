cb26cdb25b8b78a120c81d51734d614028159595
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-10-24 20:54:04 -0300

    Product CRUD e repository

---
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project01/controller/ProductController.java
@@ -0,0 +1,73 @@
+package br.com.diefenthaeler.matheus.aws_project01.controller;
+
+import br.com.diefenthaeler.matheus.aws_project01.model.Product;
+import br.com.diefenthaeler.matheus.aws_project01.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> findAll() {
+        Iterable<Product> all = repository.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable long id) {
+        Optional<Product> optProduct = repository.findById(id);
+        return optProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product productCreated = repository.save(product);
+
+        return new ResponseEntity<Product>(productCreated, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("id") long id) {
+
+        if (repository.existsById(id)){
+            product.setId(id);
+
+            Product productUpdated = repository.save(product);
+
+            return new ResponseEntity<Product>(productUpdated, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") long id) {
+
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+
+            repository.delete(product);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/bycode")
+    public ResponseEntity<Product> findByCode(@RequestParam String code) {
+        Optional<Product> optionalProduct = repository.findByCode(code);
+        return optionalProduct
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
---
+++ src/main/java/br/com/diefenthaeler/matheus/aws_project01/model/Product.java
@@ -0,0 +1,31 @@
+package br.com.diefenthaeler.matheus.aws_project01.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"code"})})
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 24, nullable = false)
+    private String model;
+
+    @Column(length = 8, nullable = false)
+    private String code;
+
+    private float price;
+
+    @Column(nullable = false)
+    private String color;
+
+}
